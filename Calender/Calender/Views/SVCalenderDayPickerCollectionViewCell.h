004fc2ac88e4783282a4b4ea1d70d35de5a3788a
Vijay <vijayakumar@hike.in>
2017-10-02 09:08:52 +0530

    Add simple reusable collection view cell

--- Calender/Calender/Views/SVCalenderDayPickerCollectionViewCell.h
+++ Calender/Calender/Views/SVCalenderDayPickerCollectionViewCell.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface SVCalenderDayPickerCollectionViewCell : UICollectionViewCell
-
+@property (nonatomic) UILabel *titleLabel;
 @end
--- Calender/Calender/Views/SVCalenderDayPickerCollectionViewCell.m
+++ Calender/Calender/Views/SVCalenderDayPickerCollectionViewCell.m
@@ -7,7 +7,36 @@
 //
 
 #import "SVCalenderDayPickerCollectionViewCell.h"
+static const CGFloat SVCalenderDayPickerCellLabelSize = 30.0f;
+
 
 @implementation SVCalenderDayPickerCollectionViewCell
 
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self configureSubViews];
+    }
+    return self;
+}
+
+- (void)configureSubViews {
+    _titleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    [self.contentView addSubview:_titleLabel];
+    [self.titleLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.titleLabel setBackgroundColor:[UIColor clearColor]];
+    self.titleLabel.layer.cornerRadius = SVCalenderDayPickerCellLabelSize/2;
+    self.titleLabel.layer.masksToBounds = YES;
+
+    //Constraints
+    [self.titleLabel.widthAnchor constraintEqualToConstant:SVCalenderDayPickerCellLabelSize].active = YES;
+    [self.titleLabel.heightAnchor constraintEqualToConstant:SVCalenderDayPickerCellLabelSize].active = YES;
+    [self.titleLabel.centerXAnchor constraintEqualToAnchor:self.contentView.centerXAnchor].active = YES;
+    [self.titleLabel.centerYAnchor constraintEqualToAnchor:self.contentView.centerYAnchor].active = YES;
+}
+
+- (void)setSelected:(BOOL)selected {
+    [super setSelected:selected];
+    //Set the custom color 
+}
 @end
